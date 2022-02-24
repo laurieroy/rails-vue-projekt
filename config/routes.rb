ed6ce3f0712c653f2a97090e54e9819c7d249e88
lauretteroy <laurieroy.dev@gmail.com>
2022-02-23 17:53:31 -0800

    Add devise reg controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  as :user do
+    put '/users/confirmation' => 'confirmations#update', via: :patch, as: :update_user_confirmation
+  end
+  
+  devise_for :users, controllers: {
+    registrations: 'registrations',
+    confirmations: 'confirmations'
+  }
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
