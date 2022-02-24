bf6d050e992d67936f85a36e99b31e3b95d55f24
lauretteroy <laurieroy.dev@gmail.com>
2022-02-23 16:48:42 -0800

    Initial setup, add devise, bulma

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
   config.action_mailer.raise_delivery_errors = false
+  
+  # For devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
